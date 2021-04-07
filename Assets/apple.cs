017a96aac640d2932d04dd4f22b7b42e21ff1f9c
eremsimsek <eremsmsk@gmail.com>
2021-04-07 16:42:18 +0300

    Inıtial Commit

---
+++ Assets/apple.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class apple : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI txtScore;
+    int score = 0;
+
+    hareket kuyrukSayisi;
+
+    private void Start()
+    {
+        kuyrukSayisi = GameObject.Find("bas").GetComponent<hareket>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "bas")
+        {
+            score += 10;
+            txtScore.text = "SKOR " + score;
+
+            koorDegis();
+            kuyrukSayisi.kuyrukAttir();
+        }
+
+        if (other.gameObject.tag =="kuyruk")
+        {
+            koorDegis();
+        }
+
+    }
+
+    void koorDegis()
+    {
+        float x = Random.Range(-12.5f, 12.5f);
+        float z = Random.Range(-5.5f, 8.5f);
+
+        transform.position = new Vector3(x, -1.5f, z);
+
+    }
+}
---
+++ Assets/hareket.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hareket : MonoBehaviour
+{
+    public GameObject kuyruk;
+    public GameObject GameOver;
+    List<GameObject> kuyruklar;
+
+    Vector3 oncekiKoordinat;
+    GameObject oncekiKuyruk;
+
+    float hiz = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        kuyruklar = new List<GameObject>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject yeniKuyruk = Instantiate(kuyruk, transform.position, Quaternion.identity);
+            kuyruklar.Add(yeniKuyruk);
+        }
+
+        InvokeRepeating("hareketEt", 0.0f, 0.1f);
+    }
+
+    void hareketEt()
+    {
+        oncekiKoordinat = transform.position;
+
+        transform.Translate(0, 0, hiz * Time.deltaTime);
+
+        kuyrukTakip();
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="duvar")
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+
+        if (other.gameObject.tag == "kuyruk")
+        {
+            GameOver.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void restart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void kuyrukAttir()
+    {
+        GameObject yeniKuyruk = Instantiate(kuyruk, transform.position, Quaternion.identity);
+        kuyruklar.Add(yeniKuyruk);
+    }
+
+    void kuyrukTakip()
+    {
+        kuyruklar[0].transform.position = oncekiKoordinat;
+        oncekiKuyruk = kuyruklar[0];
+        kuyruklar.RemoveAt(0);
+        kuyruklar.Add(oncekiKuyruk);
+    }
+
+    public void dondur(float aci)
+    {
+        transform.Rotate(0, aci, 0);
+    }
+}
